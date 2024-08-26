25980957aa324e652a6af1a42677ff34dc6f5ca3
mostafa fallaha <moostafafallaha@gmail.com>
2024-08-26 13:41:33 +0300

    updated script (ps1) to check the main branch name

--- pull_version.ps1
+++ pull_version.ps1
@@ -7,9 +7,20 @@ if (-not $Tag) {
     exit 1
 }
 
+# List of possible branch names
+$branches = @("main", "master") # you can add more here
+$mainBranch
+
+foreach ($branch in $branches) {
+    $branchExists = git rev-parse --verify --quiet $branch
+    if ($branchExists) {
+        $mainBranch = $branch
+    }
+}
+
 try {
     # Run the specified Python script with the commit message
-    python commands.py $Tag
+    python commands.py $Tag $mainBranch
 } catch {
     Write-Host "An error occurred: $($_.Exception.Message)"
     exit 1
